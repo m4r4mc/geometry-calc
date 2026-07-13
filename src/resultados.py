6517adbb6a067fa69b4a6faa95add90d1dfe3360
m4r4mc <aymaramarincorrales@gmail.com>
2026-07-12 22:24:04 -0600

    Agregar funciones para crear archivo txt con resultados

--- src/resultados.py
+++ src/resultados.py
@@ -1,1 +1,14 @@
+#Modulo para guardar los resultados en un archivo de texto
 
+def resultados(rslt, nom_archivo):
+    with open(nom_archivo, "w") as archivo:
+        archivo.write("RESULTADOS - CALCULADORA GEOMETRICA")
+
+        for i in enumerate(resultados, start=1)
+            archivo.write(f"Figura {i}: {r['nombre']}\n")
+            archivo.write("________________________________") #separar textos para que se vea mas ordenado
+            archivo.write(f"{r['formulas']['area']}\n")
+            archivo.write(f"Area: {r['area']:.4f} {r['unidad']}^2\n\n")
+            archivo.write(f"{r['formulas']['volumen']}\n")
+            archivo.write(f"Volumen: {r['volumen']:.4f} {r['unidad']}^3\n")
+        archivo.write("Gracias por usar la calculadora!")
