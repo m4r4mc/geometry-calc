ce06542ffdbb5ebf0cb97c99fe01cc9110d480bf
m4r4mc <aymaramarincorrales@gmail.com>
2026-07-12 21:51:00 -0600

    Codigo de volumen y area de cubo, figuras.py

--- src/figuras.py
+++ src/figuras.py
@@ -1,1 +1,12 @@
+#Carpeta de figuras geometricas
+#Funciones para calcular area y volumen de solidos
 
+def calcular_cubo(lado):
+    area = 6*(lado**2)
+    volumen=lado**3
+
+    formulas = {
+        "area": "Area = 6 * lado^2",
+        "volumen": "Volumen = lado^3"
+    }
+    return area, volumen, formulas
