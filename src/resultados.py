3acc0347cde6149bfcfe1ef053e18bbf458f1de5
m4r4mc <aymaramarincorrales@gmail.com>
2026-07-12 22:32:48 -0600

    Corregir errores, variables no definidas, :

--- src/resultados.py
+++ src/resultados.py
@@ -4,11 +4,11 @@ def guardar_resultados(rslt, nom_archivo):
     with open(nom_archivo, "w") as archivo:
         archivo.write("RESULTADOS - CALCULADORA GEOMETRICA")
 
-        for i in enumerate(rslt, start=1)
-            archivo.write(f"Figura {i}: {r['nombre']}\n")
+        for i in enumerate(rslt, start=1):
+            archivo.write(f"Figura {i}: {['nombre']}\n")
             archivo.write("________________________________") #separar textos para que se vea mas ordenado
-            archivo.write(f"{r['formulas']['area']}\n")
-            archivo.write(f"Area: {r['area']:.4f} {r['unidad']}^2\n\n")
-            archivo.write(f"{r['formulas']['volumen']}\n")
-            archivo.write(f"Volumen: {r['volumen']:.4f} {r['unidad']}^3\n")
+            archivo.write(f"{['formulas']['area']}\n")
+            archivo.write(f"Area: {['area']:.4f} {['unidad']}^2\n\n")
+            archivo.write(f"{['formulas']['volumen']}\n")
+            archivo.write(f"Volumen: {['volumen']:.4f} {['unidad']}^3\n")
         archivo.write("Gracias por usar la calculadora!")
