ea3e991816d085409c4dba7aed0cc3b65f3c6fe8
m4r4mc <aymaramarincorrales@gmail.com>
2026-07-13 22:03:44 -0600

    Cambio en for para escribir archivo txt con resultados, incluir if para dar densidad y masa

--- src/resultados.py
+++ src/resultados.py
@@ -4,11 +4,20 @@ def guardar_resultados(rslt, nom_archivo):
     with open(nom_archivo, "w") as archivo:
         archivo.write("RESULTADOS - CALCULADORA GEOMETRICA")
 
-        for i in enumerate(rslt, start=1):
-            archivo.write(f"Figura {i}: {['nombre']}\n")
-            archivo.write("________________________________") #separar textos para que se vea mas ordenado
-            archivo.write(f"{['formulas']['area']}\n")
-            archivo.write(f"Area: {['area']} {['unidad']}^2\n\n")
-            archivo.write(f"{['formulas']['volumen']}\n")
-            archivo.write(f"Volumen: {['volumen']} {['unidad']}^3\n")
-        archivo.write("Gracias por usar la calculadora!")
+        contador = 1
+        for i in rslt:
+            archivo.write(f"Figura {contador}: {i['nombre']}\n")
+            archivo.write("______________________________\n") #separar textos para que se vea mas ordenado
+            archivo.write(f"{i['formulas']['area']}\n")
+            archivo.write(f"Area: {i['area']} {i['unidad']}^2\n\n")
+            archivo.write(f"{i['formulas']['volumen']}\n")
+            archivo.write(f"Volumen: {i['volumen']} {i['unidad']}^3\n")
+
+            if i["material"]:
+                archivo.write(f"\n Material: {i['material']} \n")
+                archivo.write(f"Densidad: {i['densidad']} kg/m^3 \n")
+                archivo.write(f"Masa: {i['masa']} kg\n")
+            
+            contador = contador +1
+
+
